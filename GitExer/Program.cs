2590366ce5d3f69b5ef63f3c2111ff702d7dfc34
Junhwan Kim <kjh1024@snu.ac.kr>
2021-09-09 18:45:56 +0900

    second commit

--- GitExer/Program.cs
+++ GitExer/Program.cs
@@ -7,6 +7,7 @@ namespace GitExer
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World2!");
+            Console.WriteLine("World is Mine");
         }
     }
 }
